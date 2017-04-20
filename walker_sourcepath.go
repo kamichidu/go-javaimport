36cbad0bd1842e3069df42b11ce78c42e7ea7c2d
kamichidu <c.kamunagi@gmail.com>
2017-04-20 19:08:56 +0900

    only walk for future implementation

--- walker_sourcepath.go
+++ walker_sourcepath.go
@@ -1,11 +1,22 @@
 package main
 
+import (
+	"os"
+	"path/filepath"
+)
+
 type sourceWalker struct {
 	Directory string
 }
 
 func (self *sourceWalker) Walk(c *ctx) error {
-	return nil
+	return filepath.Walk(self.Directory, func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			return nil
+		}
+		// TODO: waiting for implementing java parser
+		return nil
+	})
 }
 
 var _ walker = (*sourceWalker)(nil)
