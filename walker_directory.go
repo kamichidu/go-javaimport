122375ddba15a78a46b1fb48e22c7ec1468e81e2
kamichidu <c.kamunagi@gmail.com>
2017-04-20 18:56:18 +0900

    add directory walker

--- walker_directory.go
+++ walker_directory.go
@@ -1,11 +1,30 @@
 package main
 
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/kamichidu/go-jclass"
+)
+
 type directoryWalker struct {
 	Directory string
 }
 
 func (self *directoryWalker) Walk(c *ctx) error {
-	return nil
+	return filepath.Walk(self.Directory, func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			return nil
+		}
+
+		class, err := jclass.NewJavaClassFromFilename(path)
+		if err != nil {
+			c.Logger().Printf("Can't create class object: %s", err)
+			return nil
+		}
+		c.Emitter().Emit(class)
+		return nil
+	})
 }
 
 var _ walker = (*directoryWalker)(nil)
